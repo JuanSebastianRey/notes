163a9e3575f8cc477446c962c20591a99b72dfee
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-10-26 16:35:44 -0500

    feat: :sparkles: Home-empty screen finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,36 @@
 import './Home.css'
+import search from '../../assets/img/search.png'
+import info from '../../assets/img/info_outline.png'
+import center from '../../assets/img/rafiki.png'
+import add from '../../assets/img/add.png'
 
-export default function Home( ) {
+export default function Home() {
     return (
-        <div>
-            <h1>Welcome to the Vite React App!</h1>
-        </div>
+        <main className='home'>
+            <header className='container-home'>
+                <div className='card-home'>
+                    <div className='text-card-home'>
+                        <h1>Notes</h1>
+                    </div>
+                    <div className='img-card-home'>
+                        <div>
+                            <img src={search} alt="Search" />
+                        </div>
+                        <div>
+                            <img src={info} alt="info" />
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <article className='container-note'>
+                <img src={center} alt="" />
+                <h2>Create your first note!</h2>
+            </article>
+            <footer className='container-add'>
+                <div>
+                    <img src={add} alt="" />
+                </div>
+            </footer>
+        </main>
     )
 }
